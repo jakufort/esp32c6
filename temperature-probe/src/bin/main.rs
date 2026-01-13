73ecb148019d4781b15abe781e824f89e2ed510b
Jakub Fortunka <jakufort@gmail.com>
2026-01-14 00:35:42 +0100

    not working peripherals

--- temperature-probe/src/bin/main.rs
+++ temperature-probe/src/bin/main.rs
@@ -7,10 +7,17 @@
 )]
 #![deny(clippy::large_stack_frames)]
 
+use core::cell::RefCell;
 use defmt::info;
+use embedded_graphics::mono_font::ascii::FONT_6X10;
+use embedded_graphics::mono_font::MonoTextStyleBuilder;
+use embedded_graphics::pixelcolor::BinaryColor;
+use embedded_hal_bus::i2c::RefCellDevice;
 use esp_hal::clock::CpuClock;
+use esp_hal::i2c::master::{Config, I2c};
 use esp_hal::main;
-use esp_hal::time::{Duration, Instant};
+use esp_hal::time::Rate;
+use sh1106::prelude::GraphicsMode;
 use {esp_backtrace as _, esp_println as _};
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
@@ -24,11 +31,35 @@ esp_bootloader_esp_idf::esp_app_desc!();
 #[main]
 fn main() -> ! {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let _peripherals = esp_hal::init(config);
+    let peripherals = esp_hal::init(config);
+    let mut delay = esp_hal::delay::Delay::new();
+
+    let i2c_bus = I2c::new(
+        peripherals.I2C0,
+        Config::default().with_frequency(Rate::from_khz(400))
+    )
+        .unwrap()
+        .with_sda(peripherals.GPIO6)
+        .with_scl(peripherals.GPIO7);
+
+    let i2c_bus = RefCell::new(i2c_bus);
+
+    let bme_i2c = RefCellDevice::new(&i2c_bus);
+    let oled_i2c = RefCellDevice::new(&i2c_bus);
+
+    let mut bme280 = bme280::i2c::BME280::new_primary(bme_i2c);
+    bme280.init(&mut delay).expect("BME280 init failed");
+
+    let mut display = sh1106::Builder::new().connect_i2c(oled_i2c).into();
+
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+
+    info!("Started");
 
     loop {
-        info!("Hello world!");
-        let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
+        display.clear();
     }
 }
