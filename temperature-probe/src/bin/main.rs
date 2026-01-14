81c260860c53eadac5782877cb638b916fd31ac5
Jakub Fortunka <jakufort@gmail.com>
2026-01-14 23:54:59 +0100

    displaying measurements on screen

--- temperature-probe/src/bin/main.rs
+++ temperature-probe/src/bin/main.rs
@@ -15,7 +15,7 @@ use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use embedded_graphics::Drawable;
 use embedded_graphics::mono_font::ascii::FONT_6X10;
-use embedded_graphics::mono_font::MonoTextStyleBuilder;
+use embedded_graphics::mono_font::{MonoTextStyle, MonoTextStyleBuilder};
 use embedded_graphics::pixelcolor::BinaryColor;
 use embedded_graphics::prelude::*;
 use embedded_graphics::primitives::{PrimitiveStyle, Rectangle};
@@ -28,11 +28,18 @@ use esp_hal::i2c::master::{Config, I2c};
 use esp_hal::time::Rate;
 use esp_hal::timer::timg::TimerGroup;
 use mini_oled::prelude::{I2cInterface, Sh1106};
+use core::fmt::Write;
 use {esp_backtrace as _, esp_println as _};
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
+
+const TEXT_STYLE: MonoTextStyle<BinaryColor> = MonoTextStyleBuilder::new()
+    .font(&FONT_6X10)
+    .text_color(BinaryColor::On)
+    .background_color(BinaryColor::Off)
+    .build();
 
 #[allow(
     clippy::large_stack_frames,
@@ -47,39 +54,26 @@ async fn main(_spawner: Spawner) -> ! {
     let mut bme280 = bme280(&i2c, delay);
     let mut screen = screen(&i2c);
 
-    let text_style = MonoTextStyleBuilder::new()
-        .font(&FONT_6X10)
-        .text_color(BinaryColor::On)
-        .background_color(BinaryColor::Off)
-        .build();
-
     info!("Configured");
 
     loop {
         match bme280.measure(&mut delay) {
             Ok(measurement) => {
-                println!("Temp: {}", measurement.temperature);
-                println!("Humidity: {}", measurement.humidity);
-                println!("Pressure: {}", measurement.pressure);
-                Text::with_text_style(
-                    "After refactor",
-                    Point::new(0,6),
-                    text_style,
-                    TextStyle::default(),
-                )
-                    .draw(screen.get_mut_canvas())
-                    .unwrap();
+                let mut temp: heapless::String<16> = heapless::String::new();
+                write!(temp, "TEMP:     {:04.2}", measurement.temperature).unwrap();
+                write_to_screen(0, 10, &mut screen, &temp);
+
+                let mut humidity: heapless::String<16> = heapless::String::new();
+                write!(humidity, "HUMIDITY: {:04.2}", measurement.humidity).unwrap();
+                write_to_screen(0, 20, &mut screen, &humidity);
+
+                let mut pressure: heapless::String<18> = heapless::String::new();
+                write!(pressure, "PRESSURE: {:06.2}", measurement.pressure).unwrap();
+                write_to_screen(0, 30, &mut screen, &pressure);
             }
             Err(_e) => {
                 println!("Error");
-                Text::with_text_style(
-                    "Failed",
-                    Point::new(0,6),
-                    text_style,
-                    TextStyle::default(),
-                )
-                    .draw(screen.get_mut_canvas())
-                    .unwrap();
+                write_to_screen(0, 6, &mut screen, "Failed");
             }
         }
 
@@ -144,4 +138,15 @@ fn screen<'a>(i2c: &'a Mutex<RefCell<I2c<'a, Blocking>>>) -> Sh1106<I2cInterface
     info!("screen initialized!");
 
     screen
+}
+
+fn write_to_screen<'a>(x: i32, y: i32, screen: &mut Sh1106<I2cInterface<CriticalSectionDevice<'a, I2c<'a, Blocking>>>>, text: &str) {
+    Text::with_text_style(
+        &text,
+        Point::new(x,y),
+        TEXT_STYLE,
+        TextStyle::default(),
+    )
+        .draw(screen.get_mut_canvas())
+        .unwrap();
 }
