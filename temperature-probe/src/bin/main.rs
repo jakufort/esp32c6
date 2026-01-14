6f3b476fc403452be367e2b400854f5a7193c1cf
Jakub Fortunka <jakufort@gmail.com>
2026-01-14 02:09:54 +0100

    mess, shared bus not working
    but gets measurements if OLED screen is not connected

--- temperature-probe/src/bin/main.rs
+++ temperature-probe/src/bin/main.rs
@@ -8,16 +8,23 @@
 #![deny(clippy::large_stack_frames)]
 
 use core::cell::RefCell;
-use defmt::info;
+use bme280::i2c::BME280;
+use critical_section::Mutex;
+use defmt::{error, info, println};
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+use embedded_graphics::Drawable;
 use embedded_graphics::mono_font::ascii::FONT_6X10;
 use embedded_graphics::mono_font::MonoTextStyleBuilder;
 use embedded_graphics::pixelcolor::BinaryColor;
-use embedded_hal_bus::i2c::RefCellDevice;
+use embedded_graphics::prelude::Point;
+use embedded_graphics::text::{Alignment, Text};
+use embedded_hal_bus::i2c::CriticalSectionDevice;
 use esp_hal::clock::CpuClock;
 use esp_hal::i2c::master::{Config, I2c};
-use esp_hal::main;
 use esp_hal::time::Rate;
-use sh1106::prelude::GraphicsMode;
+use esp_hal::timer::timg::TimerGroup;
+use mini_oled::prelude::{DisplayRotation, I2cInterface, Sh1106};
 use {esp_backtrace as _, esp_println as _};
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
@@ -28,10 +35,18 @@ esp_bootloader_esp_idf::esp_app_desc!();
     clippy::large_stack_frames,
     reason = "it's not unusual to allocate larger buffers etc. in main"
 )]
-#[main]
-fn main() -> ! {
+#[esp_rtos::main]
+async fn main(spawner: Spawner) -> ! {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let sw_interrupt =
+        esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+    esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
+
+    info!("Embassy initialized!");
+
     let mut delay = esp_hal::delay::Delay::new();
 
     let i2c_bus = I2c::new(
@@ -42,24 +57,65 @@ fn main() -> ! {
         .with_sda(peripherals.GPIO6)
         .with_scl(peripherals.GPIO7);
 
-    let i2c_bus = RefCell::new(i2c_bus);
+    info!("i2c initialized!");
 
-    let bme_i2c = RefCellDevice::new(&i2c_bus);
-    let oled_i2c = RefCellDevice::new(&i2c_bus);
+    let i2c_ref_cell = RefCell::new(i2c_bus);
+    let i2c_mutex = Mutex::new(i2c_ref_cell);
 
-    let mut bme280 = bme280::i2c::BME280::new_primary(bme_i2c);
-    bme280.init(&mut delay).expect("BME280 init failed");
+    let bme_i2c = CriticalSectionDevice::new(&i2c_mutex);
+    let mut bme280 = BME280::new_primary(bme_i2c);
 
-    let mut display = sh1106::Builder::new().connect_i2c(oled_i2c).into();
+    bme280.init(&mut delay).unwrap_or_else(|err| {
+        info!("init failed: {}", defmt::Debug2Format(&err));
+        panic!("Couldn't init bme280!")
+    });
 
-    let text_style = MonoTextStyleBuilder::new()
-        .font(&FONT_6X10)
-        .text_color(BinaryColor::On)
-        .build();
+    // let display_i2c = CriticalSectionDevice::new(&i2c_mutex);
+    //
+    // let i2c_interface = I2cInterface::new(display_i2c, 0x3C);
+    //
+    // let mut screen = Sh1106::new(i2c_interface);
+    // screen.init().unwrap();
+    //
+    // screen.set_rotation(DisplayRotation::Rotate180).unwrap();
+    //
+    // let text_style = MonoTextStyleBuilder::new()
+    //     .font(&FONT_6X10)
+    //     .text_color(BinaryColor::On)
+    //     .background_color(BinaryColor::Off)
+    //     .build();
 
     info!("Started");
 
     loop {
-        display.clear();
+        match bme280.measure(&mut delay) {
+            Ok(measurement) => {
+                println!("Temp: {}", measurement.temperature);
+                println!("Humidity: {}", measurement.humidity);
+                println!("Pressure: {}", measurement.pressure);
+                // Text::with_alignment(
+                //     "Got measurement",
+                //     Point::new(20,20),
+                //     text_style,
+                //     Alignment::Center,
+                // )
+                //     .draw(screen.get_mut_canvas())
+                //     .unwrap();
+            }
+            Err(e) => {
+                println!("Error");
+                // Text::with_alignment(
+                //     "Failed",
+                //     Point::new(20,30),
+                //     text_style,
+                //     Alignment::Center
+                // )
+                //     .draw(screen.get_mut_canvas())
+                //     .unwrap();
+            }
+        }
+
+        // screen.flush().unwrap();
+        Timer::after(Duration::from_secs(1)).await;
     }
 }
